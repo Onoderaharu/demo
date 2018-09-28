c06523292a58d5ac928e6e0db68a7c771fa9bbb4
lilingli <631878520@qq.com>
2018-09-28 17:32:31 +0800

    第一次提交

---
+++ demo2/src/main/java/com/lll/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.lll.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@RequestMapping("info")
+	public String info(){
+		return "HelloWorld";
+		
+	}
+}
